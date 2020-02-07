5ba7da773f2b38ce7fe39c4ca19aac3655a0588a
Ajinkya Jain <jainajinkya92@gmail.com>
2020-02-07 17:36:59 -0600

    Updated test_learned_model script

--- magic/lstm/test_learned_model.py
+++ magic/lstm/test_learned_model.py
@@ -3,8 +3,9 @@ import torch
 import argparse
 import matplotlib
 
-from ArticulationModelLearning.magic.lstm.dataset import ArticulationDataset
-from ArticulationModelLearning.magic.lstm.models import KinematicLSTMv0
+from ArticulationModelLearning.magic.lstm.dataset import ArticulationDataset, ArticulationDatasetV1, \
+    RigidTransformDataset
+from ArticulationModelLearning.magic.lstm.models import KinematicLSTMv0, KinematicLSTMv1, RigidTransformV0
 from ArticulationModelLearning.magic.lstm.utils import dual_quaternion_to_screw_batch_mode
 
 matplotlib.use('Agg')
@@ -22,26 +23,48 @@ if __name__ == "__main__":
     parser.add_argument('--nwork', type=int, default=8, help='num_workers')
     parser.add_argument('--device', type=int, default=0, help='cuda device')
     parser.add_argument('--dual-quat', action='store_true', default=False, help='Dual quaternion representation or not')
+    parser.add_argument('--model-type', type=str, default='lstm', help='lstm, rt, lstm_rt')
+
     args = parser.parse_args()
-
-    testset = ArticulationDataset(args.ntest,
-                                  args.test_dir,
-                                  n_dof=args.ndof)
-
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch,
-                                             shuffle=True, num_workers=args.nwork,
-                                             pin_memory=True)
 
     if torch.cuda.is_available():
         device = torch.device(args.device)
     else:
         device = torch.device('cpu')
 
-    # load model
-    best_model = KinematicLSTMv0(lstm_hidden_dim=1000, n_lstm_hidden_layers=1, h_fc_dim=256, n_output=120)
-    best_model.load_state_dict(torch.load(args.model_dir + args.model_name + '.net'))
-    best_model.float().to(device)
-    best_model.eval()
+    if args.model_type == 'lstm':
+        testset = ArticulationDataset(args.ntest,
+                                      args.test_dir,
+                                      n_dof=args.ndof)
+        # load model
+        best_model = KinematicLSTMv0(lstm_hidden_dim=1000, n_lstm_hidden_layers=1, h_fc_dim=256, n_output=120)
+        best_model.load_state_dict(torch.load(args.model_dir + args.model_name + '.net'))
+        best_model.float().to(device)
+        best_model.eval()
+
+    elif args.model_type == 'rt':
+        testset = RigidTransformDataset(args.ntest,
+                                        args.test_dir,
+                                        n_dof=args.ndof)
+        # load model
+        best_model = RigidTransformV0(n_output=120)
+        best_model.load_state_dict(torch.load(args.model_dir + args.model_name + '.net'))
+        best_model.float().to(device)
+        best_model.eval()
+
+    elif args.model_type == 'lstm_rt':
+        testset = ArticulationDatasetV1(args.ntest,
+                                        args.test_dir,
+                                        n_dof=args.ndof)
+        # load model
+        best_model = KinematicLSTMv1(lstm_hidden_dim=1000, n_lstm_hidden_layers=1, h_fc_dim=256, n_output=120)
+        best_model.load_state_dict(torch.load(args.model_dir + args.model_name + '.net'))
+        best_model.float().to(device)
+        best_model.eval()
+
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch,
+                                             shuffle=True, num_workers=args.nwork,
+                                             pin_memory=True)
 
     all_l_hat_err = torch.empty(0)
     all_m_err = torch.empty(0)
@@ -60,29 +83,36 @@ if __name__ == "__main__":
             y_pred = best_model(depth)
             y_pred = y_pred.view(y_pred.size(0), -1, 8)
 
+            if args.model_type in ['rt', 'lstm_rt']:
+                labels = labels.view(labels.size(0), -1, 8)
+
             if args.dual_quat:
                 y_pred = dual_quaternion_to_screw_batch_mode(y_pred)
                 labels = dual_quaternion_to_screw_batch_mode(labels)
 
             err = labels - y_pred
-            #all_l_hat_err = torch.cat(
-             #   (all_l_hat_err, torch.mean(torch.norm(err[:, :, :3], dim=-1), dim=-1).cpu()))
-            #all_m_err = torch.cat((all_m_err, torch.mean(torch.norm(err[:, :, 3:6], dim=-1), dim=-1).cpu()))
-            l_hat_std, l_hat_mean = torch.std_mean(torch.acos(torch.mul(labels[:,:,:3], y_pred[:, :, :3]).sum(dim=-1)/(torch.norm(labels[:,:,:3], dim=-1)*torch.norm(y_pred[:,:,:3], dim=-1))), dim=-1)
+            # all_l_hat_err = torch.cat(
+            #   (all_l_hat_err, torch.mean(torch.norm(err[:, :, :3], dim=-1), dim=-1).cpu()))
+            # all_m_err = torch.cat((all_m_err, torch.mean(torch.norm(err[:, :, 3:6], dim=-1), dim=-1).cpu()))
+            l_hat_std, l_hat_mean = torch.std_mean(torch.acos(
+                torch.mul(labels[:, :, :3], y_pred[:, :, :3]).sum(dim=-1) / (
+                            torch.norm(labels[:, :, :3], dim=-1) * torch.norm(y_pred[:, :, :3], dim=-1))), dim=-1)
             all_l_hat_err = torch.cat((all_l_hat_err, l_hat_mean.cpu()))
 
-            m_std, m_mean = torch.std_mean(torch.norm(labels[:, :, 3:6], dim=-1) - torch.norm(y_pred[:, :, 3:6], dim=-1) , dim=-1)
-            m_std_abs, m_mean_abs = torch.std_mean(torch.abs(torch.norm(labels[:, :, 3:6], dim=-1) - torch.norm(y_pred[:, :, 3:6], dim=-1)) , dim=-1)
+            m_std, m_mean = torch.std_mean(
+                torch.norm(labels[:, :, 3:6], dim=-1) - torch.norm(y_pred[:, :, 3:6], dim=-1), dim=-1)
+            m_std_abs, m_mean_abs = torch.std_mean(
+                torch.abs(torch.norm(labels[:, :, 3:6], dim=-1) - torch.norm(y_pred[:, :, 3:6], dim=-1)), dim=-1)
             all_m_err = torch.cat((all_m_err, m_mean.cpu()))
             all_m_err_abs = torch.cat((all_m_err_abs, m_mean_abs.cpu()))
 
             all_q_err = torch.cat((all_q_err, torch.mean(err[:, :, 6], dim=-1).cpu()))
             all_d_err = torch.cat((all_d_err, torch.mean(err[:, :, 7], dim=-1).cpu()))
 
-            #all_l_hat_std = torch.cat(
+            # all_l_hat_std = torch.cat(
             #    (all_l_hat_std, torch.std(torch.norm(err[:, :, :3], dim=-1), dim=-1).cpu()))
-            #all_m_std = torch.cat((all_m_std, torch.std(torch.norm(err[:, :, 3:6], dim=-1), dim=-1).cpu()))
-            
+            # all_m_std = torch.cat((all_m_std, torch.std(torch.norm(err[:, :, 3:6], dim=-1), dim=-1).cpu()))
+
             all_l_hat_std = torch.cat((all_l_hat_std, l_hat_std.cpu()))
             all_m_std = torch.cat((all_m_std, m_std.cpu()))
             all_m_std_abs = torch.cat((all_m_std_abs, m_std_abs.cpu()))
@@ -93,7 +123,8 @@ if __name__ == "__main__":
     x_axis = np.arange(all_l_hat_err.size(0))
 
     fig = plt.figure(1)
-    plt.errorbar(x_axis, all_l_hat_err.numpy(), all_l_hat_std.numpy(), marker='o', mfc='blue', ms=4., capsize=3., capthick=1.)
+    plt.errorbar(x_axis, all_l_hat_err.numpy(), all_l_hat_std.numpy(), marker='o', mfc='blue', ms=4., capsize=3.,
+                 capthick=1.)
     plt.xlabel("Test object number")
     plt.ylabel("Angle error (rad)")
     plt.title("Test error in screw axis orientation")
@@ -111,7 +142,8 @@ if __name__ == "__main__":
     plt.close(fig)
 
     fig = plt.figure(2)
-    plt.errorbar(x_axis, all_m_err_abs.numpy(), all_m_std_abs.numpy(), marker='o', mfc='blue', ms=4, capsize=3., capthick=1.)
+    plt.errorbar(x_axis, all_m_err_abs.numpy(), all_m_std_abs.numpy(), marker='o', mfc='blue', ms=4, capsize=3.,
+                 capthick=1.)
     plt.xlabel("Test object number")
     plt.ylabel("Absolute Error")
     plt.title("Absolute test error in norm(m)")
@@ -120,7 +152,7 @@ if __name__ == "__main__":
     plt.close(fig)
 
     fig = plt.figure(3)
-    plt.errorbar(x_axis, all_q_err.numpy(), all_q_std.numpy(),  capsize=3., capthick=1.)
+    plt.errorbar(x_axis, all_q_err.numpy(), all_q_std.numpy(), capsize=3., capthick=1.)
     plt.xlabel("Test object number")
     plt.ylabel("Error")
     plt.title("Test error in theta")
