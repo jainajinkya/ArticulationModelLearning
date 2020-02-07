ea2cf03a05835d8c84bd4dbfb5c9f848a5d22c71
Ajinkya Jain <jainajinkya92@gmail.com>
2020-02-07 17:30:32 -0600

    Initial commit to see seq+2img learning

--- magic/lstm/dataset.py
+++ magic/lstm/dataset.py
@@ -1,5 +1,5 @@
 import os
-from itertools import permutations
+from itertools import permutations, combinations
 
 import dq3d
 import numpy as np
@@ -67,6 +67,59 @@ class ArticulationDataset(Dataset):
             #                               np.array([dq.dual.w, dq.dual.x, dq.dual.y, dq.dual.z])))
 
         label = torch.from_numpy(label).float()
+        sample = {'depth': depth_imgs,
+                  'label': label}
+
+        return sample
+
+
+''' LSTM + 2 images'''
+
+
+class ArticulationDatasetV1(Dataset):
+    def __init__(self,
+                 ntrain,
+                 root_dir,
+                 n_dof):
+        super(ArticulationDatasetV1, self).__init__()
+
+        self.root_dir = root_dir
+        self.labels_data = None
+        self.length = ntrain
+        self.n_dof = n_dof
+        self.pair_idxs = list(combinations(range(16), r=2))
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        if self.labels_data is None:
+            self.labels_data = h5py.File(os.path.join(self.root_dir, 'complete_data.hdf5'), 'r')
+
+        obj_idx = int(idx / len(self.pair_idxs))
+        pair_idx = self.pair_idxs[idx % len(self.pair_idxs)]
+
+        # One Sample for us corresponds to one instantiation of an object type
+        obj_data = self.labels_data['obj_' + str(obj_idx).zfill(6)]
+
+        # Load depth image
+        depth_imgs = torch.from_numpy(np.concatenate((obj_data['depth_imgs'],
+                                                      np.expand_dims(obj_data['depth_imgs'][pair_idx[0]], axis=0),
+                                                      np.expand_dims(obj_data['depth_imgs'][pair_idx[1]], axis=0))))
+
+        depth_imgs.unsqueeze_(1).float()
+        depth_imgs = torch.cat((depth_imgs, depth_imgs, depth_imgs), dim=1)
+
+        # Load labels
+        pt1 = obj_data['moving_frame_in_world'][pair_idx[0], :]
+        pt2 = obj_data['moving_frame_in_world'][pair_idx[1], :]
+        pt1_T_pt2 = change_frames(pt1, pt2)
+
+        # Generating labels in screw notation: label := <l_hat, m, theta, d> = <3, 3, 1, 1>
+        l_hat, m, theta, d = transform_to_screw(translation=pt1_T_pt2[:3],
+                                                quat_in_wxyz=pt1_T_pt2[3:])
+
+        label = torch.from_numpy(np.concatenate((l_hat, m, [theta], [d]))).float()
         sample = {'depth': depth_imgs,
                   'label': label}
 
--- magic/lstm/train_lstm.py
+++ magic/lstm/train_lstm.py
@@ -1,9 +1,10 @@
 import argparse
 
 import torch
-from ArticulationModelLearning.magic.lstm.dataset import ArticulationDataset, RigidTransformDataset
+from ArticulationModelLearning.magic.lstm.dataset import ArticulationDataset, RigidTransformDataset, \
+    ArticulationDatasetV1
 from ArticulationModelLearning.magic.lstm.model_trainer import ModelTrainer
-from ArticulationModelLearning.magic.lstm.models import KinematicLSTMv0, RigidTransformV0
+from ArticulationModelLearning.magic.lstm.models import KinematicLSTMv0, RigidTransformV0, KinematicLSTMv1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train object learner on articulated object dataset.")
@@ -24,27 +25,50 @@ if __name__ == "__main__":
     parser.add_argument('--obj', type=str, default='microwave')
     parser.add_argument('--drop_p', type=float, default=0.5, help='dropout prob')
     parser.add_argument('--device', type=int, default=0, help='cuda device')
+    parser.add_argument('--model-type', type=str, default='lstm', help='lstm, rt, lstm_rt')
     args = parser.parse_args()
 
     print(args)
     print('cuda?', torch.cuda.is_available())
 
-    # trainset = ArticulationDataset(args.ntrain,
-    #                                args.train_dir,
-    #                                n_dof=args.ndof)
-    #
-    # testset = ArticulationDataset(args.ntest,
-    #                               args.test_dir,
-    #                               n_dof=args.ndof)
+    if args.model_type == 'lstm':
+        trainset = ArticulationDataset(args.ntrain,
+                                       args.train_dir,
+                                       n_dof=args.ndof)
 
-    '''Rigid Transform Datasets'''
-    trainset = RigidTransformDataset(args.ntrain,
-                                     args.train_dir,
-                                     n_dof=args.ndof)
+        testset = ArticulationDataset(args.ntest,
+                                      args.test_dir,
+                                      n_dof=args.ndof)
 
-    testset = RigidTransformDataset(args.ntest,
-                                    args.test_dir,
-                                    n_dof=args.ndof)
+        # init model
+        network = KinematicLSTMv0(lstm_hidden_dim=1000, n_lstm_hidden_layers=1,
+                                drop_p=args.drop_p, h_fc_dim=256, n_output=120)
+
+    elif args.model_type == 'rt':
+        '''Rigid Transform Datasets'''
+        trainset = RigidTransformDataset(args.ntrain,
+                                         args.train_dir,
+                                         n_dof=args.ndof)
+
+        testset = RigidTransformDataset(args.ntest,
+                                        args.test_dir,
+                                        n_dof=args.ndof)
+
+        network = RigidTransformV0(drop_p=args.drop_p, n_output=8)
+
+    elif args.model_type == 'lstm_rt':
+        ## Sequence and 2 images
+        trainset = ArticulationDatasetV1(args.ntrain,
+                                       args.train_dir,
+                                       n_dof=args.ndof)
+
+        testset = ArticulationDatasetV1(args.ntest,
+                                      args.test_dir,
+                                      n_dof=args.ndof)
+
+        network = KinematicLSTMv1(lstm_hidden_dim=1000, n_lstm_hidden_layers=1,
+                                  drop_p=args.drop_p, h_fc_dim=256, n_output=8)
+
 
     testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch,
                                              shuffle=True, num_workers=args.nwork,
@@ -53,13 +77,6 @@ if __name__ == "__main__":
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch,
                                               shuffle=True, num_workers=args.nwork,
                                               pin_memory=True)
-
-    # init model
-    # network = KinematicLSTMv0(lstm_hidden_dim=1000, n_lstm_hidden_layers=1,
-    #                           drop_p=args.drop_p, h_fc_dim=256, n_output=120)
-
-    '''Rigid Transform Model'''
-    network = RigidTransformV0(drop_p=args.drop_p, n_output=8)
 
     # setup trainer
     if torch.cuda.is_available():
