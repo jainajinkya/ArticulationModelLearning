Small bug fix in args of train_model. Changed model for lstm_2_imgs to 1000 units in fc2

magic/lstm/models.py
@@ -82,8 +82,10 @@ class KinematicLSTMv1(nn.Module):
 
         # self.fc1 = nn.Linear((16 + 2) * self.lstm_hidden_dim, self.h_fc_dim)
         self.fc1 = nn.Linear(3*self.lstm_hidden_dim, self.h_fc_dim)
-        self.fc2 = nn.Linear(self.h_fc_dim, 256)
-        self.fc3 = nn.Linear(256, self.n_output)
+        #self.fc2 = nn.Linear(self.h_fc_dim, 256)
+        #self.fc3 = nn.Linear(256, self.n_output)
+        self.fc2 = nn.Linear(self.h_fc_dim, 1000)
+        self.fc3 = nn.Linear(1000, self.n_output)
 
     def forward(self, X_3d, Y_in):
         # X shape: Batch x Sequence x 3 Channels x img_dims