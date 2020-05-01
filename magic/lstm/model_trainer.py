c6d2964be87c50b9a84d8850672ad51ca0b64c69
Ajinkya Jain <jainajinkya92@gmail.com>
2020-05-01 17:06:34 +0530

    Plotting gradient flow through the network

--- magic/lstm/model_trainer.py
+++ magic/lstm/model_trainer.py
@@ -93,6 +93,9 @@ class ModelTrainer(object):
                 self.optimizer.step()
                 running_loss += loss.item()
 
+                "Check if loss is becoming Nan "
+                self.plot_grad_flow(self.model.named_parameters())
+
         stop = time.time()
         print('Epoch %s -  Train  Loss: %.5f Time: %.5f' % (str(epoch).zfill(3),
                                                             running_loss / batches_per_dataset,
@@ -206,3 +209,35 @@ class ModelTrainer(object):
         plt.tight_layout()
         plt.savefig('plots/' + self.name + '/d_err' + fname_suffix + '.png')
         plt.close(fig)
+
+
+    def plot_grad_flow(self, named_parameters):
+        ''' Plots the gradients flowing through different layers in the net during training.
+        Can be used for checking for possible gradient vanishing / exploding problems.
+
+        Usage: Plug this function in Trainer class after loss.backwards() as
+        "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow
+        from link: https://discuss.pytorch.org/t/check-gradient-flow-in-network/15063
+        '''
+        ave_grads = []
+        max_grads = []
+        layers = []
+        for n, p in named_parameters:
+            if (p.requires_grad) and ("bias" not in n):
+                layers.append(n)
+                ave_grads.append(p.grad.abs().mean())
+                max_grads.append(p.grad.abs().max())
+        plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+        plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+        plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
+        plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+        plt.xlim(left=0, right=len(ave_grads))
+        plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+        plt.xlabel("Layers")
+        plt.ylabel("average gradient")
+        plt.title("Gradient flow")
+        plt.grid(True)
+        plt.legend([Line2D([0], [0], color="c", lw=4),
+                    Line2D([0], [0], color="b", lw=4),
+                    Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
+        plt.savefig('plots/' + self.name + '/grad_flow.png')
