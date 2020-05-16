ef22b0083f7eda1243d09bf7bd333d3299d6b4c6
Ajinkya Jain <jainajinkya92@gmail.com>
2020-05-16 18:29:06 +0530

    Changed test_learned_model script to test baselines as well

--- magic/lstm/test_learned_model.py
+++ magic/lstm/test_learned_model.py
@@ -1,12 +1,16 @@
 import argparse
+import os
 
+import GeneralizingKinematics.magic.mixture.mdn as mdn
 import matplotlib
 import numpy as np
 import torch
-from ArticulationModelLearning.magic.lstm.dataset import ArticulationDataset, ArticulationDatasetV1, \
-    RigidTransformDataset
-from ArticulationModelLearning.magic.lstm.models import KinematicLSTMv0, KinematicLSTMv1, RigidTransformV0, DeepArtModel
-from ArticulationModelLearning.magic.lstm.utils import dual_quaternion_to_screw_batch_mode, distance_bw_plucker_lines
+from ArticulationModelLearning.magic.lstm.dataset import ArticulationDataset
+from ArticulationModelLearning.magic.lstm.models import DeepArtModel
+from ArticulationModelLearning.magic.lstm.utils import distance_bw_plucker_lines
+from GeneralizingKinematics.magic.mixture.dataset import MixtureDataset
+from GeneralizingKinematics.magic.mixture.models import KinematicMDNv3
+from GeneralizingKinematics.magic.mixture.utils import *
 
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
@@ -24,168 +28,185 @@ if __name__ == "__main__":
     parser.add_argument('--nwork', type=int, default=8, help='num_workers')
     parser.add_argument('--device', type=int, default=0, help='cuda device')
     parser.add_argument('--dual-quat', action='store_true', default=False, help='Dual quaternion representation or not')
-    parser.add_argument('--model-type', type=str, default='lstm', help='lstm, rt, lstm_rt')
+    parser.add_argument('--model-type', type=str, default='ours', help='ours, ben, li')
     parser.add_argument('--load-wts', action='store_true', default=False, help='Should load model wts from prior run?')
 
     args = parser.parse_args()
 
     ntest = args.ntest * args.aug_multi
 
+    # Dataset for common axis
+    all_ori_err_mean = torch.empty(0)
+    all_ori_err_std = torch.empty(0)
+    all_dist_err_mean = torch.empty(0)
+    all_dist_err_std = torch.empty(0)
+
+    output_dir = os.path.join(args.output_dir, args.model_type, args.model_name)
+
     if torch.cuda.is_available():
         device = torch.device(args.device)
     else:
         device = torch.device('cpu')
 
-    if args.model_type == 'rt':
-        testset = RigidTransformDataset(ntest,
-                                        args.test_dir,
-                                        n_dof=args.ndof)
-        # load model
-        best_model = RigidTransformV0(n_output=8)
-        best_model.load_state_dict(torch.load(args.model_dir + args.model_name + '.net'))
+    if args.model_type == 'ben':
+        print("Testing Model: Ben et al.")
+        testset = MixtureDataset(ntest,
+                                 args.test_dir,
+                                 n_dof=args.ndof,
+                                 normalize=True)
+
+        best_model = KinematicMDNv3(n_gaussians=20,
+                                    out_features=testset.labels.shape[1])
+        best_model.load_state_dict(torch.load(os.path.join(args.model_dir, args.model_name + '.net')))
         best_model.float().to(device)
         best_model.eval()
 
-    elif args.model_type == 'lstm_rt':
-        testset = ArticulationDatasetV1(ntest,
-                                        args.test_dir,
-                                        n_dof=args.ndof)
-        # load model
-        best_model = KinematicLSTMv1(lstm_hidden_dim=1000, n_lstm_hidden_layers=1, h_fc_dim=256, n_output=8)
-        best_model.load_state_dict(torch.load(args.model_dir + args.model_name + '.net'))
-        best_model.float().to(device)
-        best_model.eval()
+        testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch,
+                                                 shuffle=False, num_workers=args.nwork,
+                                                 pin_memory=True)
+        with torch.no_grad():
+            for X in testloader:
+                depth, labels = X['depth'].to(device), X['label'].to(device)
+                pi, sigma, mu = best_model(depth)
+
+        output = mdn.sample(pi, sigma, mu)
+
+        # expand labels and output back to full dataset size
+        output = expand_labels(testloader.dataset.full_labels[:len(labels)],
+                               output,
+                               testloader.dataset.keep_columns,
+                               testloader.dataset.one_columns)
+
+        labels = expand_labels(testloader.dataset.full_labels[:len(labels)],
+                               labels,
+                               testloader.dataset.keep_columns,
+                               testloader.dataset.one_columns)
+
+        # interpret and convert to real.
+        bounds = np.load(os.path.join(args.test_dir, 'bounds.npy'))
+        ground_truth_params = convert_dict_to_real(interpret_labels(labels, args.ndof), bounds, args.ndof)
+        param_dict = convert_dict_to_real(interpret_labels(output, args.ndof), bounds, args.ndof)
+
+        real_axis = ground_truth_params['axis']
+        real_net_axis = param_dict['axis']
+
+        all_dist_err_std, all_dist_err_mean = torch.std_mean(
+            torch.norm(real_axis[:, :, :3] - real_net_axis[:, :, :3], dim=-1), dim=-1).cpu()
+
+        all_ori_err_std, all_ori_err_mean = torch.zeros_like(all_dist_err_std), torch.zeros_like(all_dist_err_std)
+
+        # Configuration error
+        real_configs = ground_truth_params['config']
+        net_configs = param_dict['config']
+
+        all_q_err_std, all_q_err_mean = torch.std_mean(
+            torch.norm(real_configs[:, :, :] - net_configs[:, :, :], dim=-1), dim=-1).cpu()
+
+        x_axis = np.arange(all_q_err_mean.size(0))
+        fig = plt.figure(3)
+        plt.errorbar(x_axis, all_q_err_mean.numpy(), all_q_err_std.numpy(), capsize=3., capthick=1., ls='none')
+        plt.xlabel("Test object number")
+        plt.ylabel("Error in Config")
+        plt.title("Test error in Configurations")
+        plt.tight_layout()
+        plt.savefig(output_dir + '/theta_err.png')
+        plt.close(fig)
+
+    elif args.model_type == 'li':
+        print("Testing Model: Li et al.")
 
     else:
-        testset = ArticulationDataset(ntest,
-                                      args.test_dir,
-                                      n_dof=args.ndof)
+        print("Testing Model: Ours")
+
         # load model
         # best_model = KinematicLSTMv0(lstm_hidden_dim=1000, n_lstm_hidden_layers=1, h_fc_dim=256, n_output=8)
         best_model = DeepArtModel(lstm_hidden_dim=1000, n_lstm_hidden_layers=1, h_fc_dim=256, n_output=8)
-        best_model.load_state_dict(torch.load(args.model_dir + args.model_name + '.net'))
+        best_model.load_state_dict(torch.load(os.path.join(args.model_dir, args.model_name + '.net')))
         best_model.float().to(device)
         best_model.eval()
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch,
-                                             shuffle=False, num_workers=args.nwork,
-                                             pin_memory=True)
+        testset = ArticulationDataset(ntest,
+                                      args.test_dir,
+                                      n_dof=args.ndof)
 
-    # all_l_hat_err = torch.empty(0)
-    # all_m_err = torch.empty(0)
-    # all_m_err_abs = torch.empty(0)
-    # all_q_err = torch.empty(0)
-    # all_d_err = torch.empty(0)
-    # all_l_hat_std = torch.empty(0)
-    # all_m_std = torch.empty(0)
-    # all_m_std_abs = torch.empty(0)
-    # all_q_std = torch.empty(0)
-    # all_d_std = torch.empty(0)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch,
+                                                 shuffle=False, num_workers=args.nwork,
+                                                 pin_memory=True)
 
-    all_ori_err_mean = torch.empty(0)
-    all_ori_err_std = torch.empty(0)
-    all_dist_err_mean = torch.empty(0)
-    all_dist_err_std = torch.empty(0)
-    all_q_mean = torch.empty(0)
-    all_q_std = torch.empty(0)
-    all_d_mean = torch.empty(0)
-    all_d_std = torch.empty(0)
+        all_q_err_mean = torch.empty(0)
+        all_q_err_std = torch.empty(0)
+        all_d_err_mean = torch.empty(0)
+        all_d_err_std = torch.empty(0)
 
-    obj_idxs = torch.empty(0)  # Recording object indexes for analysis
+        obj_idxs = torch.empty(0)  # Recording object indexes for analysis
 
-    # Data collection for particle filter testing
-    all_labels = torch.empty(0)
-    all_preds = torch.empty(0)
-    all_errs = torch.empty(0)
+        # # Data collection for particle filter testing
+        # all_labels = torch.empty(0)
+        # all_preds = torch.empty(0)
+        # all_errs = torch.empty(0)
 
-    with torch.no_grad():
-        # for X, batch_idxs in testloader:
-        for X in testloader:
-            if args.model_type == 'lstm_rt':
-                depth, all_labels, labels = X['depth'].to(device), \
-                                            X['all_labels'].to(device), \
-                                            X['label'].to(device)
-                y_pred = best_model(depth, all_labels)
-            else:
+        with torch.no_grad():
+            for X in testloader:
                 depth, labels = X['depth'].to(device), X['label'].to(device)
                 y_pred = best_model(depth)
-
-            y_pred = y_pred.view(y_pred.size(0), -1, 8)
-
-            if args.model_type in ['rt', 'lstm_rt']:
-                labels = labels.view(labels.size(0), -1, 8)
-            if args.model_type == 'lstm':
+                y_pred = y_pred.view(y_pred.size(0), -1, 8)
                 y_pred = y_pred[:, 1:, :]
 
-            if args.dual_quat:
-                y_pred = dual_quaternion_to_screw_batch_mode(y_pred)
-                labels = dual_quaternion_to_screw_batch_mode(labels)
+                # Orientation error
+                ori_err_std, ori_err_mean = torch.std_mean(torch.acos(
+                    torch.mul(labels[:, :, :3], y_pred[:, :, :3]).sum(dim=-1) / (
+                            torch.norm(labels[:, :, :3], dim=-1) * torch.norm(y_pred[:, :, :3], dim=-1))), dim=-1)
+                all_ori_err_mean = torch.cat((all_ori_err_mean, ori_err_mean.cpu()))
+                all_ori_err_std = torch.cat((all_ori_err_std, ori_err_std.cpu()))
 
-            # Orientation error
-            ori_err_std, ori_err_mean = torch.std_mean(torch.acos(
-                torch.mul(labels[:, :, :3], y_pred[:, :, :3]).sum(dim=-1) / (
-                        torch.norm(labels[:, :, :3], dim=-1) * torch.norm(y_pred[:, :, :3], dim=-1))), dim=-1)
-            all_ori_err_mean = torch.cat((all_ori_err_mean, ori_err_mean.cpu()))
-            all_ori_err_std = torch.cat((all_ori_err_std, ori_err_std.cpu()))
+                # Distance b/w plucker lines
+                dist_err_std, dist_err_mean = torch.std_mean(distance_bw_plucker_lines(labels, y_pred), dim=-1)
+                all_dist_err_mean = torch.cat((all_dist_err_mean, dist_err_mean.cpu()))
+                all_dist_err_std = torch.cat((all_dist_err_std, dist_err_std.cpu()))
 
-            # Distance b/w plucker lines
-            dist_err_std, dist_err_mean = torch.std_mean(distance_bw_plucker_lines(labels, y_pred), dim=-1)
-            all_dist_err_mean = torch.cat((all_dist_err_mean, dist_err_mean.cpu()))
-            all_dist_err_std = torch.cat((all_dist_err_std, dist_err_std.cpu()))
+                # Configurational errors
+                q_err_std, q_err_mean = torch.std_mean(torch.norm(labels[:, :, 6] - y_pred[:, :, 6], dim=-1), dim=-1)
+                all_q_err_mean = torch.cat((all_q_err_mean, q_err_mean.cpu()))
+                all_q_err_std = torch.cat((all_q_err_std, q_err_std.cpu()))
 
-            # Configurational errors
-            q_err_std, q_err_mean = torch.std_mean(labels[:, :, 6] - y_pred[:, :, 6], dim=-1)
-            all_q_mean = torch.cat((all_q_mean, q_err_mean.cpu()))
-            all_q_std = torch.cat((all_q_std, q_err_std.cpu()))
+                d_err_std, d_err_mean = torch.std_mean(torch.norm(labels[:, :, 7] - y_pred[:, :, 7], dim=-1), dim=-1)
+                all_d_err_mean = torch.cat((all_d_err_std, d_err_mean.cpu()))
+                all_d_err_std = torch.cat((all_d_err_std, d_err_std.cpu()))
 
-            d_err_std, d_err_mean = torch.std_mean(labels[:, :, 7] - y_pred[:, :, 7], dim=-1)
-            all_d_mean = torch.cat((all_d_std, d_err_mean.cpu()))
-            all_d_std = torch.cat((all_d_std, d_err_std.cpu()))
+                # # Data for particle filter
+                # all_labels = torch.cat((all_labels, labels.cpu()))
+                # all_preds = torch.cat((all_preds, y_pred.cpu()))
+                # all_errs = torch.cat((all_errs, (labels - y_pred).cpu()))
 
-            # # err = labels - y_pred
-            # # all_l_hat_err = torch.cat(
-            # #   (all_l_hat_err, torch.mean(torch.norm(err[:, :, :3], dim=-1), dim=-1).cpu()))
-            # # all_m_err = torch.cat((all_m_err, torch.mean(torch.norm(err[:, :, 3:6], dim=-1), dim=-1).cpu()))
-            # l_hat_std, l_hat_mean = torch.std_mean(torch.acos(
-            #     torch.mul(labels[:, :, :3], y_pred[:, :, :3]).sum(dim=-1) / (
-            #             torch.norm(labels[:, :, :3], dim=-1) * torch.norm(y_pred[:, :, :3], dim=-1))), dim=-1)
-            # all_l_hat_err = torch.cat((all_l_hat_err, l_hat_mean.cpu()))
-            #
-            # # m_std, m_mean = torch.std_mean(
-            # #     torch.norm(labels[:, :, 3:6], dim=-1) - torch.norm(y_pred[:, :, 3:6], dim=-1), dim=-1)
-            # # m_std_abs, m_mean_abs = torch.std_mean(
-            # #     torch.abs(torch.norm(labels[:, :, 3:6], dim=-1) - torch.norm(y_pred[:, :, 3:6], dim=-1)), dim=-1)
-            # m_std, m_mean = torch.std_mean(torch.norm(err[:, :, 3:6], dim=-1), dim=-1)
-            # all_m_err = torch.cat((all_m_err, m_mean.cpu()))
-            # # all_m_err_abs = torch.cat((all_m_err_abs, m_mean_abs.cpu()))
-            #
-            # all_q_err = torch.cat((all_q_err, torch.mean(err[:, :, 6], dim=-1).cpu()))
-            # all_d_err = torch.cat((all_d_err, torch.mean(err[:, :, 7], dim=-1).cpu()))
-            #
-            # # all_l_hat_std = torch.cat(
-            # #    (all_l_hat_std, torch.std(torch.norm(err[:, :, :3], dim=-1), dim=-1).cpu()))
-            # # all_m_std = torch.cat((all_m_std, torch.std(torch.norm(err[:, :, 3:6], dim=-1), dim=-1).cpu()))
-            #
-            # all_l_hat_std = torch.cat((all_l_hat_std, l_hat_std.cpu()))
-            # all_m_std = torch.cat((all_m_std, m_std.cpu()))
-            # # all_m_std_abs = torch.cat((all_m_std_abs, m_std_abs.cpu()))
-            #
-            #
-            # all_q_std = torch.cat((all_q_std, torch.std(err[:, :, 6], dim=-1).cpu()))
-            # all_d_std = torch.cat((all_d_std, torch.std(err[:, :, 7], dim=-1).cpu()))
+        # Sort objects as per the idxs
+        x_axis = np.arange(all_q_err_mean.size(0))
 
-            # obj_idxs = torch.cat((obj_idxs, batch_idxs.cpu().float()))
+        fig = plt.figure(3)
+        plt.errorbar(x_axis, all_q_err_mean.numpy(), all_q_err_std.numpy(), capsize=3., capthick=1., ls='none')
+        plt.xlabel("Test object number")
+        plt.ylabel("Error (rad)")
+        plt.title("Test error in theta")
+        plt.tight_layout()
+        plt.savefig(output_dir + '/theta_err.png')
+        plt.close(fig)
 
-            # Data for particle filter
-            all_labels = torch.cat((all_labels, labels.cpu()))
-            all_preds = torch.cat((all_preds, y_pred.cpu()))
-            all_errs = torch.cat((all_errs, (labels - y_pred).cpu()))
+        fig = plt.figure(4)
+        plt.errorbar(x_axis, all_d_err_mean.numpy(), all_d_err_std.numpy(), capsize=3., capthick=1., ls='none')
+        plt.xlabel("Test object number")
+        plt.ylabel("Error (m)")
+        plt.title("Test error in d")
+        plt.tight_layout()
+        plt.savefig(output_dir + '/d_err.png')
+        plt.close(fig)
 
+        # # Storing data for particle filter analysis
+        # p_data = {'labels': all_labels.numpy(), 'predictions': all_preds.numpy(), 'errors': all_errs.numpy()}
+        # import pickle
+        # pickle.dump(p_data, open(output_dir + '/test_prediction_data.pkl', 'wb'))
+
+    """ Common Plots"""
     # Plot variation of screw axis
-    output_dir = args.output_dir + args.model_name
-    x_axis = np.arange(all_q_mean.size(0))
-
-    # Sort objects as per the idxs
+    x_axis = np.arange(all_ori_err_mean.size(0))
     fig = plt.figure(1)
     plt.errorbar(x_axis, all_ori_err_mean.numpy(), all_ori_err_std.numpy(), marker='o', mfc='blue', ms=4., capsize=3.,
                  capthick=1., ls='none')
@@ -205,75 +226,3 @@ if __name__ == "__main__":
     plt.tight_layout()
     plt.savefig(output_dir + '/distance_test_error.png')
     plt.close(fig)
-
-    fig = plt.figure(3)
-    plt.errorbar(x_axis, all_q_mean.numpy(), all_q_std.numpy(), capsize=3., capthick=1., ls='none')
-    plt.xlabel("Test object number")
-    plt.ylabel("Error (rad)")
-    plt.title("Test error in theta")
-    plt.tight_layout()
-    plt.savefig(output_dir + '/theta_err.png')
-    plt.close(fig)
-
-    fig = plt.figure(4)
-    plt.errorbar(x_axis, all_d_mean.numpy(), all_d_std.numpy(), capsize=3., capthick=1., ls='none')
-    plt.xlabel("Test object number")
-    plt.ylabel("Error (m)")
-    plt.title("Test error in d")
-    plt.tight_layout()
-    plt.savefig(output_dir + '/d_err.png')
-    plt.close(fig)
-
-    # # Sort objects as per the idxs
-    # fig = plt.figure(1)
-    # plt.errorbar(x_axis, all_l_hat_err.numpy(), all_l_hat_std.numpy(), marker='o', mfc='blue', ms=4., capsize=3.,
-    #              capthick=1.)
-    # plt.xlabel("Test object number")
-    # plt.ylabel("Angle error (rad)")
-    # plt.title("Test error in screw axis orientation")
-    # plt.tight_layout()
-    # plt.savefig(output_dir + '/l_hat_err_angle.png')
-    # plt.close(fig)
-    #
-    # fig = plt.figure(2)
-    # plt.errorbar(x_axis, all_m_err.numpy(), all_m_std.numpy(), marker='o', ms=4, mfc='blue', capsize=3., capthick=1.)
-    # plt.xlabel("Test object number")
-    # plt.ylabel("Error")
-    # plt.title("Test error in norm(m)")
-    # plt.tight_layout()
-    # plt.savefig(output_dir + '/m_err_norm.png')
-    # plt.close(fig)
-
-    """
-    fig = plt.figure(2)
-    plt.errorbar(x_axis, all_m_err_abs.numpy(), all_m_std_abs.numpy(), marker='o', mfc='blue', ms=4, capsize=3.,
-                 capthick=1.)
-    plt.xlabel("Test object number")
-    plt.ylabel("Absolute Error")
-    plt.title("Absolute test error in norm(m)")
-    plt.tight_layout()
-    plt.savefig(output_dir + '/m_err_abs_norm.png')
-    plt.close(fig)
-    """
-    # fig = plt.figure(3)
-    # plt.errorbar(x_axis, all_q_err.numpy(), all_q_std.numpy(), capsize=3., capthick=1.)
-    # plt.xlabel("Test object number")
-    # plt.ylabel("Error")
-    # plt.title("Test error in theta")
-    # plt.tight_layout()
-    # plt.savefig(output_dir + '/theta_err.png')
-    # plt.close(fig)
-    #
-    # fig = plt.figure(4)
-    # plt.errorbar(x_axis, all_d_err.numpy(), all_d_std.numpy(), capsize=3., capthick=1.)
-    # plt.xlabel("Test object number")
-    # plt.ylabel("Error")
-    # plt.title("Test error in d")
-    # plt.tight_layout()
-    # plt.savefig(output_dir + '/d_err.png')
-    # plt.close(fig)
-
-    # # Storing data for particle filter analysis
-    # p_data = {'labels': all_labels.numpy(), 'predictions': all_preds.numpy(), 'errors': all_errs.numpy()}
-    # import pickle
-    # pickle.dump(p_data, open(output_dir + '/test_prediction_data.pkl', 'wb'))
