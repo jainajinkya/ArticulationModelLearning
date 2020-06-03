ea8326ce3fcfd1e1254cb11b52c6c3c88cac2967
Ajinkya Jain <jainajinkya92@gmail.com>
2020-06-03 16:55:47 +0530

    minor bug fix for data device

--- magic/lstm/utils.py
+++ magic/lstm/utils.py
@@ -198,8 +198,8 @@ def theta_config_error(target, prediction):
     rot_tar = angle_axis_to_rotation_matrix(tar_[:, :3], tar_[:, 6])
     rot_pred = angle_axis_to_rotation_matrix(pred_[:, :3], pred_[:, 6])
     I_ = torch.eye(3).reshape((1, 3, 3))
-    I_ = I_.repeat(rot_tar.size(0), 1, 1)
-    return torch.norm(I_ - torch.bmm(rot_pred, rot_tar.transpose(1, 2)).cpu(), dim=(1, 2)).view(target.shape[:2])
+    I_ = I_.repeat(rot_tar.size(0), 1, 1).to(target.device)
+    return torch.norm(I_ - torch.bmm(rot_pred, rot_tar.transpose(1, 2)), dim=(1, 2), p=2).view(target.shape[:2])
 
 
 def angle_axis_to_rotation_matrix(angle_axis, theta, eps=1e-10):
